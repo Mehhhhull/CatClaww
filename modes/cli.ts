68744dc1812891e0d216ffd33ee47636b12b0151
Mehhhhull <mehulkumarsingh.2004@gmail.com>
2026-06-14 00:41:13 +0530

    Added CLI Sub modes

--- modes/cli.ts
+++ modes/cli.ts
@@ -2,4 +2,31 @@ import { Chalk } from "chalk";
 import {select,isCancel} from "@clack/prompts";
 
 
-export async function runCliMode(){}
+export async function runCliMode(){
+  while(true){
+    const mode=await select({
+      message:"Choose CLI Sub-mode",
+      options:[
+        {value:"agent",label:"Agent Mode"},
+        {value:"plan",label:"Plan Mode"},
+        {value:"ask",label:"Ask Mode"},
+        {value:"back",label:"Back to main menu"},
+      ],
+    });
+    if(isCancel(mode) || mode=="back") return;
+
+    if(mode=="agent"){
+      console.log("Agent")
+    }
+    if(mode=="ask"){
+      console.log("Ask")
+    }
+    if(mode=="plan"){
+      console.log("Plan")
+    }
+
+    if( mode !== 'agent' && mode !=='plan' && mode !== 'ask'){
+      console.log(chalk.red('\n That mode is not implemented yet. \n'));
+    }
+  }
+}
